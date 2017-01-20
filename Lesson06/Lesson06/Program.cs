06ca763b9064de9698a89b2a25b6df2ead021a94
Dennis Kleiner <alex.nevskiy1987@gmail.com>
2017-01-20 05:53:56 +0200

    Collection

---
+++ Lesson06/Lesson06/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLesson5
+{
+    class Collection
+    {
+        int[] nums;
+        int pos;
+
+        // Note: Default constructor - a constructer that gets no parameters.
+        public Collection() : this(10) { }
+
+        public Collection(int size)
+        {
+            nums = new int[size];
+            pos = 0;
+        }
+
+        public void Add(int number)
+        {
+            makeRoom();
+            nums[pos++] = number;
+        }
+
+        void makeRoom()
+        {
+            if (pos == nums.Length) //no room
+            {
+                int[] temp = new int[nums.Length * 2];
+                for (int i = 0; i < pos; i++)
+                {
+                    temp[i] = nums[i];
+                }
+                nums = temp;
+            }
+        }
+
+        void checkIndex(int index)
+        {
+            if (index >= pos || index < 0)
+            {
+                throw new Exception("Index out of bounds");
+            }
+        }
+
+        public int Get(int index)
+        {
+            checkIndex(index);
+
+            return nums[index];
+        }
+
+        public void Remove(int index)
+        {
+            checkIndex(index);
+
+            for (int i = index; i < pos; i++)
+                nums[i] = nums[i + 1]; ;
+
+            pos--;
+        }
+
+        public int Size()
+        {
+            return pos;
+        }
+
+        public int[] ToArray()
+        {
+            int[] temp = new int[pos];
+            for (int i = 0; i < pos; i++)
+            {
+                temp[i] = nums[i];
+            }
+            return temp;
+        }
+
+        public int IndexOf(int number)
+        {
+            for (int i = 0; i < pos; i++)
+            {
+                if (nums[i] == number)
+                    return i;
+            }
+            return -1;
+        }
+
+        public void Insert(int number, int index)
+        {
+            checkIndex(index);
+            makeRoom();
+            for (int i = pos; i > index; i--)
+            {
+                nums[i] = nums[i - 1];
+            }
+            nums[index] = number;
+            pos++;
+        }
+
+        public void Set(int number, int index)
+        {
+            checkIndex(index);
+            nums[index] = number;
+        }
+        //homework: the below functions returns true if the other
+        // collection values equal to this one.
+        public bool Equals(Collection other)
+        {
+            return true;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int q = Quotient(7, 0);
+                Console.WriteLine("q=" + q);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("boom! " + ex.Message);
+            }
+            Collection myCol1 = new Collection();
+            for (int i = 0; i < 9; i++)
+            {
+                myCol1.Add(i * 3);
+                Console.WriteLine(myCol1.Get(i));
+            }
+            myCol1.Insert(-1, 8);
+            Console.WriteLine("-------------------");
+            for (int i = 0; i < myCol1.Size(); i++)
+            {
+                Console.WriteLine(myCol1.Get(i));
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+
+        }
+
+        static int Quotient(int x, int y)
+        {
+            // Note: "Exception" is a reserved class in c sharp (all c sharp programs will have exception).
+            // it is often used as reference to other programmers when they use your functions.
+            if (y == 0)
+            {
+                throw new Exception("division by zero");
+            }
+            int sum = y;
+            int result = 0;
+            while (sum <= x)
+            {
+                result++;
+                sum += y;
+            }
+            return result++;
+        }
+    }
+}
---
+++ Lesson06/Lesson06/TeacherCode.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson05
+{
+
+
+    class Collection
+    {
+        int[] arr;
+        int pos;
+        public Collection()
+        {
+            arr = new int[10];
+            pos = 0;
+
+        }
+
+        //adds an element to the collection
+        //this function always succeeds
+        public void Add(int x)
+        {
+
+        }
+
+        //returns an element at the position index
+        public int Get(int index)
+        {
+            return 0;
+        }
+
+        //removes an element from the collection in the position index
+        public void Remove(int index)
+        {
+
+        }
+    }
+
+    class Car
+    {
+        public Car()
+        {
+            Console.WriteLine("in constructor");
+        }
+
+    }
+
+
+    class DivisionResult
+    {
+        public int quotient;
+        public int remainder;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection coll = new Collection();
+            coll.Add(93);
+
+            Car c = new Car();
+
+            DivisionResult result = division(7, 3);
+            if (result != null)
+            {
+                Console.WriteLine("3 fits in 7 " + result.quotient
+                    + " times, with a remainder of " +
+                    result.remainder);
+            }
+            else
+            {
+                Console.WriteLine("error occured");
+            }
+
+        }
+
+        static int distance(int x, int y)
+        {
+            int small = x;
+            int large = y;
+            if (x > y)
+            {
+                small = y;
+                large = x;
+            }
+            int result = 0;
+            while (small + result < large)
+                result++;
+            return result;
+        }
+
+        static int product(int x, int y)
+        {
+            int small = x;
+            int large = y;
+            if (x > y)
+            {
+                small = y;
+                large = x;
+            }
+            int result = 0;
+            for (int i = 0; i < small; i++)
+                result += large;
+            return result;
+        }
+        static int quotient(int x, int y)
+        {
+            if (y == 0)
+                return -1;
+            int result = 0;
+            int sum = y;
+            while (sum <= x)
+            {
+                result++;
+                sum += y;
+            }
+            return result;
+        }
+
+        static int remainder(int x, int y)
+        {
+            if (y == 0)
+                return -1;
+            return distance(x, product(quotient(x, y), y));
+        }
+
+        static DivisionResult division(int x, int y)
+        {
+            if (y == 0)
+            {
+                /*
+                DivisionResult error = new DivisionResult();
+                error.quotient = -1;
+                return error;
+                */
+                return null;
+            }
+            int result = 0;
+            int sum = y;
+            int temp = 0;
+            while (sum <= x)
+            {
+                result++;
+                sum += y;
+                temp += y;
+            }
+            DivisionResult divisionResult = new DivisionResult();
+            divisionResult.quotient = result;
+            divisionResult.remainder = distance(x, temp);
+
+            return divisionResult;
+        }
+    }
+}
