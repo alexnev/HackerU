a2fb145b268316814386e29a119f632533ad01cf
Dennis Kleiner <alex.nevskiy1987@gmail.com>
2017-01-27 19:38:34 +0200

    Events. Summary of creating listeners. Try, Catch, Finally

---
+++ Lesson20/Lesson20/TryCatch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcepetionHandling
+{
+    class Program
+    {
+        //building your own exception classes
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(divide(8, 2));
+
+                return;
+            }
+            //you can use multiple catches with similiar logic to "if else"
+            catch (DivisionByZeroException ex)
+            {
+                Console.WriteLine("oops " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oops " + ex.Message);
+            }
+            //finally activates even if you have a return command.
+            //used to remind to do full closure to objects that demand cleaning 
+            finally
+            {
+                Console.WriteLine("Finally");
+
+                Console.ReadKey();
+            }
+            Console.WriteLine("Done");
+
+            Console.ReadKey();
+        }
+
+        static float divide(float x, float y)
+        {
+            if (y == 0)
+                throw new DivisionByZeroException();
+            return x / y;
+        }
+    }
+
+    class DivisionByZeroException : Exception
+    {
+        DateTime exceptionTimeStamp;
+        public DivisionByZeroException() : base("division by zero")
+        {
+            exceptionTimeStamp = DateTime.Now;
+        }
+    }
+}
