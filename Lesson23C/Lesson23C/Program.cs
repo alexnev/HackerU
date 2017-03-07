170c512e3657cf74c26c48be3736b05f001b08ca
Dennis Kleiner <alex.nevskiy1987@gmail.com>
2017-03-07 20:37:17 +0200

    TCP Client

---
+++ Lesson23C/Lesson23C/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson23C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //It's very important to close open connections such as client and stream when the application is done.
+            TcpClient client = new TcpClient("127.0.0.1", 3000);
+            NetworkStream stream = client.GetStream();
+            String s = "";
+            byte[] bytes = new UTF8Encoding().GetBytes(s);
+            stream.Write(bytes, 0, bytes.Length);
+            byte[] buffer = new byte[1024];
+            int actuallyRead = stream.Read(buffer, 0, buffer.Length);
+            string responseFromServer = new UTF8Encoding().GetString(buffer, 0, actuallyRead);
+            stream.Close();
+            client.Close();
+        }
+    }
+}
